fedcab18654f1c1e24480ffee8dcdabfe8676acb
Sneha <snehapatidar0033@gmail.com>
2026-03-14 16:13:27 +0530

    leetcode solutions

--- strings/palindrome.py
+++ strings/palindrome.py
@@ -1,8 +1,9 @@
 def isPalindrome(s):
-    n=''
-    for i in s:
-        if i.isalnum():
-            n+=i.lower()
-    return n==n[::-1]
+    s.lower()
+    o=[]
+    for a in s:
+        if ord(a)>=97 and ord(a)<=122:
+            o.append(a)
+    return o==o[::-1] 
 
-print(isPalindrome('caracc'))     
+print(isPalindrome("A man, a plan, a canal: Panama"))     
